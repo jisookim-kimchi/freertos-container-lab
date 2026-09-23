37c9e1df948e4f051a1ce37884f0dc6417e24f32
jisookim-kimchi <wltn249@gmail.com>
2026-09-23 10:04:03 +0200

    dts modified

--- QEMU_virt.sh
+++ QEMU_virt.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
 
-qemu-system-aarch64 -M virt -cpu cortex-a72 -machine dumpdtb=virt.dtb
-dtc -I dtb -O dts -o virt.dts virt.dtb
+dtc -I dts -O dtb -o virt.dtb virt.dts
--- core0-2_linux/run_linux.sh
+++ core0-2_linux/run_linux.sh
@@ -4,6 +4,7 @@ qemu-system-aarch64 \
     -M virt \
     -cpu cortex-a72 \
     -m 2G \
+    -dtb ../virt.dtb \
     -object memory-backend-file,id=mb,size=2G,mem-path=/dev/shm/qemu_amp_shmem,share=on \
     -numa node,memdev=mb \
     -smp 2 \
